c9ac4a9a43cbc33ef3cf304ba74bb0fb78ba1982
Christianm9000 <112858793+Christianm9000@users.noreply.github.com>
2022-09-18 22:06:59 +0200

    TicTacToe2.0

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,10 @@ board = [1,2,3,4,5,6,7,8,9] #Board as a string, otherwise board = [[1,2,3], [4,5
 userinput = 0 #The users input, edited later
 userturn = 0 #userturn, defines who's turn it is.
 
+
+#Check Win Function
 def checkwin(board, win):
+    
     #Check win Horizontally
     for i in range(3):
         i *= 3 #Change the row when i is above 0
@@ -29,44 +32,74 @@ def checkwin(board, win):
     
     #Check win diagonally
     for i in range(2): #Check the 2 diagonals
-        if i == 0 and board[i] == "X" and board[i+4] == "X" and board[i+8] == "X" : #Check if player 1 wins
+        if i == 0 and board[i] == "X" and board[i+4] == "X" and board[i+8] == "X" : #Check Left to right diagonal, player 1
             print("Player 1 has won the game!")
             win = True
             break
-        if i == 1 and board[i+1] == "O" and board[i+3] == "O" and board[i+5] == "O" : #Check if player 2 wins
+        if i == 0 and board[i] == "O" and board[i+4] == "O" and board[i+8] == "O" : #Check left to right diagonal, player 2
+            print("Player 2 has won the game!")
+            win = True
+            break
+        if i == 1 and board[i+1] == "X" and board[i+3] == "X" and board[i+5] == "X" : #Check right to left diagonal, Player 1
+            print("Player 1 has won the game!")
+            win = True
+            break
+        if i == 1 and board[i+1] == "O" and board[i+3] == "O" and board[i+5] == "O" : #Check right to left diagonal, Player 2
             print("Player 2 has won the game!")
             win = True
             break
 
     return(win) #Return win value, True if game has to end.
 
+
+
+#Draw Board
 def drawboard(board):
+    print('')
+    print('')
     for i in range(3):
         i *= 3
         print(f"| {board[i]} | {board[i +1]} | {board[i +2]} |")
+    
+    print('')
+
 
 #Get Userinput 
 def user(board, userturn, win):
+
     #Define variables
     userinput = 0
     tempboard = []
+
+    #What options are available
     for i in range(9):
         if type(board[i]) == int:
             tempboard.append(board[i])
+    
+    #Check Draw
+    if len(tempboard) == 0: #If tempboard is empty (no user inputs left) call draw.
+        print("Draw!")
+        win = True
+
+    #Start Userinput
     while True:
         try:
-            if userturn % 2 == 0:
-                userinput = int(input(f"Pick one of: {tempboard}''Player 1: "))
+            if userturn % 2 == 0 and win == False:
+                print(f"Pick one of: {tempboard}")
+                userinput = int(input("Player 1: "))
                 while userinput < 1 or userinput > 9 or userinput not in tempboard:
-                    userinput = int(input(f"Pick one of: {tempboard}''Player 1: "))
+                    print(f"Pick one of: {tempboard}")
+                    userinput = int(input("Player 1: "))
                 board[userinput-1] = "X"
                 break
-            print(userturn)
 
-            if userturn % 2 != 0:
-                userinput = int(input(f"Pick one of: {tempboard}''Player 2: "))
+
+            if userturn % 2 != 0 and win == False:
+                print(f"Pick one of: {tempboard}")
+                userinput = int(input("Player 2: "))
                 while userinput < 1 or userinput > 9 or userinput not in tempboard:
-                    userinput = int(input(f"Pick one of: {tempboard}''Player 2: "))
+                    print(f"Pick one of: {tempboard}")
+                    userinput = int(input("Player 2: "))
                 board[userinput-1] = "O"
                 break
 
@@ -75,11 +108,6 @@ def user(board, userturn, win):
 
     #Draw board again
     drawboard(board)
-
-    #Check Draw
-    if len(tempboard) == 0: #If tempboard is empty (no user inputs left) call draw.
-        print("Draw!")
-        win = True
     
     #Do Next player
     userturn += 1
